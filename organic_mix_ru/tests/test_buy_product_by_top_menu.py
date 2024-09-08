23f1273017fcde9b1c725ae213a3f8edfaf3940f
antareszet <alex_aksenov@bk.ru>
2024-09-08 17:41:07 +0400

    Update test_buy_product_by_top_menu.py

--- organic_mix_ru/tests/test_buy_product_by_top_menu.py
+++ organic_mix_ru/tests/test_buy_product_by_top_menu.py
@@ -10,7 +10,7 @@ from pages.product_page import Product_page
 from pages.order_make_page import Order_make_page
 
 
-def test_buy_product_by_top_menu():  # тест покупки товара из верхнего меню
+def test_buy_product_by_top_menu():  # тест покупки товара из верхнего меню c сопледующей покупкой
     options = webdriver.ChromeOptions()
     options.add_argument("--log-level=3")
     options.add_argument('--headless')
